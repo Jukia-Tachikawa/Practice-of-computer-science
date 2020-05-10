703d04a86822b6eb17ca1b0ee57c25e0e5bfd8b2
Jukia Tachikawa <jukia@DESKTOP-VKMO45S.localdomain>
2020-05-10 21:35:47 +0900

    独自例外を作成

---
+++ src/myexception/NumberOfInputException.java
@@ -0,0 +1,12 @@
+package myexception;
+
+//入力が想定した入力個数と異なるにスローされる.
+public class NumberOfInputException extends RuntimeException{
+  public NumberOfInputException(){
+    super();
+  }
+
+  public NumberOfInputException(String message){
+    super(message);
+  }
+}
